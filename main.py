d9f0f3b7fbe3620e22626f277b1730b37fe878d1
Josmoo <52058577+josmoo@users.noreply.github.com>
2026-01-22 16:31:49 -0600

    it kinda works now but is slow. hate justified, but slowness is skill issue

--- main.py
+++ main.py
@@ -2,21 +2,33 @@ from lxml import etree
 from lxml.cssselect import CSSSelector
 import cloudscraper
 
-def dissectMovie(j):
-    print(f'Hi,')
+def ConstructDataLink(element):
+    return "https://letterboxd.com" + element.get("data-item-link")
 
-def handlePagination():
+def DissectMovies(movieList, rating):
+    movieGrid = movieList.cssselect('ul.-p70 li .react-component')
+    movieLinks = list(map(ConstructDataLink, movieGrid))
+    for movieLink in movieLinks:
+        avgRating = etree.fromstring(scraper.get(movieLink).text, parser).cssselect('meta[name="twitter:data2"]')
+        if avgRating:
+            print(avgRating[0].get('content'))
+        else:
+            print("no one watches this shit movie sorry")
     return
 
-def constructDataLink(element):
-    return "https://letterboxd.com" + element.get("data-item-link")
+def HandlePagination():
+    return
+
+#GLOBAL
+scraper = cloudscraper.create_scraper()
+parser = etree.HTMLParser()
 
 if __name__ == '__main__':
 
-    scraper = cloudscraper.create_scraper()
-    parser = etree.HTMLParser()
-    text = scraper.get("https://letterboxd.com/jomimo/films/rated/5/by/date/").text
-    html_root  = etree.fromstring(text, parser)
-    var = html_root.cssselect('ul.-p70 li .react-component')
-    var2 = list(map(constructDataLink, var))
-    print(1)
+    ratings = [".5", "1", "1.5", "2", "2.5", "3", "3.5", "4", "4.5", "5"]
+
+    for rating in ratings:
+        text = scraper.get("https://letterboxd.com/jomimo/films/rated/" + rating + "/by/rating/").text
+        html_root  = etree.fromstring(text, parser)
+        DissectMovies(html_root, rating)
+        print(1)
