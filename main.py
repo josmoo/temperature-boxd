ed25321ead42d4dc098f7722e9acd6828567f6f3
Josmoo <52058577+josmoo@users.noreply.github.com>
2026-01-22 13:33:04 -0600

    friendship ended with python requests, no cloudscraper is my best friend

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import requests
 from lxml import etree
 from lxml.cssselect import CSSSelector
-
+import cloudscraper
 
 def dissectMovie(j):
     print(f'Hi,')
@@ -10,8 +9,12 @@ def handlePagination():
     return
 
 if __name__ == '__main__':
+
+    scraper = cloudscraper.create_scraper()
+
+
     parser = etree.HTMLParser()
-    text = requests.get('https://letterboxd.com/jomimo/films/rated/5/by/date/', headers={"User-Agent": "Mozilla/5.0"}).text
+    text = scraper.get("https://letterboxd.com/jomimo/films/rated/5/by/date/").text
     html_root  = etree.fromstring(text, parser)
     print(text)
     var = html_root.cssselect('ul.grid')
